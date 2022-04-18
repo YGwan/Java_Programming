e37ca1435bf4a7af166d493dd58634796b2806b5
YGwan <50222603+YGwan@users.noreply.github.com>
2022-04-18 09:58:02 +0900

    Assignment

---
+++ src/Forth/P21.java
@@ -0,0 +1,45 @@
+/*
+노래 한 곡을 나타내는 Song 클래스를 작성하라. Song은 다음과 같은 필드로 구성된다. ● 노래의 제목을 나타내는 title
+        ● 가수를 나타내는 artist
+        ● 노래가 발표된 연도를 나타내는 year ● 국적을 표시하는 country
+        또한 Song 클래스에 다음 생성자와 메소드를 작성하라.
+        ● 생성자 2개 : 기본 생성자와 매개변소루 모든 필드를 초기화하는 생성자
+        ● 노래 정보를 출력하는 show() 메소드
+        ● main() 메소드에서 2021년, Dynamite, BTS, Korea 를 이용하여 Song 객체를 생성하고
+        show()를 이용하여 노래의 정보를 다음과 같이 출력하라.
+        */
+package Forth;
+
+public class P21 {
+
+    public static void main(String[] args) {
+
+        Song song = new Song("Dynamite", "BTS:",2021, "Korea");
+        song.show();
+
+
+    }
+}
+
+class Song {
+    String title;
+    String artist;
+
+    public Song() {
+    }
+
+    int year;
+    String country;
+
+    public Song(String title, String artist, int year, String country) {
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+        this.country = country;
+    }
+
+    public void show() {
+        //Dynamite by BTS from Korea, 2021
+        System.out.println(title+" by "+ artist+" from "+country+", "+year);
+    }
+}
